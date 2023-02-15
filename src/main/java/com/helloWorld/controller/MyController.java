6a14571f338c954b2d100c0ea28a726970060521
1043213 <1043213@INBLRLT4027.questgedc.com>
2023-02-15 15:34:11 +0530

    Added new endpoint to service

--- src/main/java/com/helloWorld/controller/MyController.java
+++ src/main/java/com/helloWorld/controller/MyController.java
@@ -1,5 +1,7 @@
 package com.helloWorld.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,12 @@ public class MyController {
 	@GetMapping("/echo-hello")
 	public String echoHello() {
 		return "Hello-world";
+	}
+	
+	@GetMapping("/echo-hello-with-date")
+	public String echoHelloDate() {
+		
+		return "Hello-world" + new Date();
 	}
 
 }
