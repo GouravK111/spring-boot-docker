ff822e14d7b3ec87e5fb10903f7d182cd1e08bfc
1043213 <1043213@INBLRLT4027.questgedc.com>
2023-02-22 12:05:19 +0530

    added deployment and service file

--- src/main/java/com/helloWorld/controller/MyController.java
+++ src/main/java/com/helloWorld/controller/MyController.java
@@ -1,7 +1,5 @@
 package com.helloWorld.controller;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +9,6 @@ public class MyController {
 	@GetMapping("/echo-hello")
 	public String echoHello() {
 		return "Hello-world";
-	}
-	
-	@GetMapping("/echo-hello-with-date")
-	public String echoHelloDate() {
-		
-		return "Hello-world" + new Date();
 	}
 
 }
